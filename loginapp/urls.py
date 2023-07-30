7d19c656affd72507360abdf6e14d75d5d71360e
Shruti Jain <shrutjain2306@gmail.com>
2023-07-30 17:07:51 +0530

    add static files

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -3,9 +3,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/mysite')),
     path('mysite/', include("mysite.urls")),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
